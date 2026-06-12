d9d1489f0c1f9683178f0190c013f5b05054cec5
Owen Saez <107427768+sjsaes@users.noreply.github.com>
2026-06-12 08:52:57 -0500

    Complete Assignment 2

---
+++ assignment 2 - Variables/variables.py
@@ -0,0 +1,17 @@
+name = "Owen Sáez"
+multiplication = 5*6
+division = 8/3
+divisionNoRemainder = 8//3
+modulo = 8%3
+addition = 4200000+23090
+subtraction = 4200000-7633
+
+def main():
+  print("The answer to your query is", multiplication)
+  print("The answer to your query is", round(division, 8))
+  print("The answer to your query is", divisionNoRemainder)
+  print("The answer to your query is", modulo)
+  print("The answer to your query is", addition)
+  print("The answer to your query is", subtraction)
+  print(name)
+main()
